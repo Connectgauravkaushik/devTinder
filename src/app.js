589cff8a1d27caa16c4d48cbd5742b8af4b9ab75
gaurav <connect.gauravkaushik@gmail.com>
2025-02-25 14:12:55 +0530

    route-handlers

--- src/app.js
+++ src/app.js
@@ -1,23 +1,18 @@
 const express = require('express')
 const app = express();// instance of express js application
 
+// app.route("/route" , [rp1 , rp2 ,rp3])
+// app.route("/route" , [rp1 , rp2] ,rp3)
 
-
-app.get("/user", (req, res) => {
-    res.send({firstName:"Gaurav" , lastName:"Kaushik"});
-});
-
-app.post("/user" , (req , res)=>{
-    res.send("Data saved successfully !");
-})
-
-app.delete("/user" , (req , res)=>{
-    res.send("user Deleted successfully");
-})
-
-app.use("/test", (req, res) => {
-    res.send("Hello from the server !");
-});
+app.get("/user", (req, res, next) => {
+    console.log("1st Route Handler")
+    next();
+    res.send("1nd Route Handler");
+}, (req, res) => {
+    console.log("2nd Route Handler")
+    res.send("2nd Route Handler");
+}
+);
 
 
 // listening the request on this port number
