4463878305e0ea761492898fe4f48ae81d7a5e1d
gaurav <connect.gauravkaushik@gmail.com>
2025-02-28 13:33:29 +0530

    Created CRUD API for DevTinder

--- src/app.js
+++ src/app.js
@@ -2,16 +2,15 @@ const express = require("express");
 const connectDB = require("./config/database");
 const User = require("./models/user");
 const app = express(); // instance of express js application
+app.use(express.json()); //Middleware Convert JSON into js object.
 
+
+
+// SignUp API for a new user
 app.post("/signup", async (req, res) => {
   // To add the new signup user , create need to create a new instance form the user model.
   // creating a new user instance of the model with the above data.
-  const user = new User({
-    firstName: "Gaurav",
-    lastName: "kaushik",
-    emailId: "gauravkaushik415@gmail.com",
-    password: "gaurav@1234",
-  });
+  const user = new User(req.body);
   try {
     // return a promise and save the data in the database / collection
     await user.save();
@@ -20,6 +19,67 @@ app.post("/signup", async (req, res) => {
     res.status(400).send("Error saving the user : " + error.message);
   }
 });
+
+
+// Get user by email
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+  try {
+    const users = await User.findOne({ emailId: userEmail });
+    if (!users) {
+      res.status(404).send("user not found !");
+    } else {
+      res.send(users);
+    }
+  } catch (error) {
+    res.status(400).send("Somewthing went wrong!");
+  }
+});
+
+
+//Feed API - get all the users from the database
+app.get("/feed", async (req, res) => {
+  try {
+    // if empty object passed in fint it will get all the object
+    const users = await User.find({});
+    res.send(users);
+  } catch (error) {
+    res.status(400).send("Somewthing went wrong!");
+  }
+});
+
+
+//deleting a user form the database using _id 
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+
+  try {
+    // if empty object passed in fint it will get all the object
+    const users = await User.findByIdAndDelete(userId);
+    res.send("user Deleted successfully");
+  } catch (error) {
+    res.status(400).send("Somewthing went wrong!");
+  }
+});
+
+
+//Updating the data in the database
+app.patch("/user", async (req, res) => {
+  const userId = req.body.userId;
+  const data = req.body;
+
+  try {
+    const user = await User.findByIdAndUpdate({ _id: userId }, data, { returnDocument: "after" })
+    console.log(user);
+    res.send("user Updated Successfully")
+
+  } catch (error) {
+    res.status(400).send("Somewthing went wrong!");
+  }
+
+});
+
+
 
 // listening the request on this port number
 connectDB()
