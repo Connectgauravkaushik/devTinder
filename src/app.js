bd20d1babaf9f8638479742697e0999b83d3f723
gaurav <connect.gauravkaushik@gmail.com>
2025-03-02 17:01:09 +0530

    API Validations and Data Sanitizations

--- src/app.js
+++ src/app.js
@@ -4,8 +4,6 @@ const User = require("./models/user");
 const app = express(); // instance of express js application
 app.use(express.json()); //Middleware Convert JSON into js object.
 
-
-
 // SignUp API for a new user
 app.post("/signup", async (req, res) => {
   // To add the new signup user , create need to create a new instance form the user model.
@@ -19,7 +17,6 @@ app.post("/signup", async (req, res) => {
     res.status(400).send("Error saving the user : " + error.message);
   }
 });
-
 
 // Get user by email
 app.get("/user", async (req, res) => {
@@ -36,7 +33,6 @@ app.get("/user", async (req, res) => {
   }
 });
 
-
 //Feed API - get all the users from the database
 app.get("/feed", async (req, res) => {
   try {
@@ -48,8 +44,7 @@ app.get("/feed", async (req, res) => {
   }
 });
 
-
-//deleting a user form the database using _id 
+//deleting a user form the database using _id
 app.delete("/user", async (req, res) => {
   const userId = req.body.userId;
 
@@ -62,24 +57,50 @@ app.delete("/user", async (req, res) => {
   }
 });
 
-
 //Updating the data in the database
-app.patch("/user", async (req, res) => {
-  const userId = req.body.userId;
+app.patch("/user/:userId", async (req, res) => {
+  const userId = req.params?.userId;
   const data = req.body;
 
   try {
-    const user = await User.findByIdAndUpdate({ _id: userId }, data, { returnDocument: "after" })
+    const ALLOWED_UPDATES = [
+      "userId",
+      "photoUrl",
+      "about",
+      "gender",
+      "age",
+      "skills",
+    ];
+
+    const invalidUpdates = Object.keys(data).filter(
+      (key) => !ALLOWED_UPDATES.includes(key)
+    );
+
+    if (data?.skills?.length > 5) {
+      throw new Error("Skills cannot be more than 5");
+    }
+
+    // If there are invalid fields, throw an error
+    if (invalidUpdates.length > 0) {
+      throw new Error(
+        `Update not allowed for the following fields: ${invalidUpdates.join(
+          ", "
+        )}`
+      );
+    }
+
+    // Proceed to update the user
+    const user = await User.findByIdAndUpdate({ _id: userId }, data, {
+      returnDocument: "after",
+      runValidators: true,
+    });
+
     console.log(user);
-    res.send("user Updated Successfully")
-
+    res.send("User updated successfully");
   } catch (error) {
-    res.status(400).send("Somewthing went wrong!");
+    res.status(400).send("Update failed: " + error.message);
   }
-
 });
-
-
 
 // listening the request on this port number
 connectDB()
--- src/models/user.js
+++ src/models/user.js
@@ -1,27 +1,76 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 //simply use this to create schema
-const userSchema = new mongoose.Schema({
-  // here we will pass all the parameter which define user.
-  firstName: {
-    type: String,
+// we will add some strict checks for field validations
+const userSchema = new mongoose.Schema(
+  {
+    // here we will pass all the parameter which define user.
+    firstName: {
+      type: String,
+      required: true, // make sure this field is mandatory.
+      unique: true,
+    },
+    lastName: {
+      type: String,
+    },
+    emailId: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true, // Ensures uniqueness of email
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Invalid Email Address !");
+        }
+      },
+      lowercase: true, // Ensures email is stored in lowercase
+      trim: true, // Removes any leading/trailing spaces
+    },
+    password: {
+      type: String,
+      required: true,
+      validate(value) {
+        if (!validator.isStrongPassword(value)) {
+          throw new Error("Enter a Strong password !");
+        }
+      },
+    },
+    age: {
+      type: Number,
+      min: [18, "Age must be at least 18"],
+      max: [100, "Age cannot exceed 100"],
+    },
+    gender: {
+      type: String,
+      //validation in build
+      validate(value) {
+        if (!["male", "female", "other"].includes(value)) {
+          throw new Error("Gender data is not valid");
+        }
+      },
+    },
+    photoUrl: {
+      type: String,
+      default:
+        "https://cdn.pixabay.com/photo/2015/03/04/22/35/avatar-659652_640.png",
+      validate(value) {
+        if (!validator.isURL(value)) {
+          throw new Error("Invalid Photo URL !", value);
+        }
+      },
+    },
+    about: {
+      type: String,
+      default: "This is a default description of the new user !",
+    },
+    skills: {
+      type: [String],
+    },
   },
-  lastName: {
-    type: String,
-  },
-  emailId: {
-    type: String,
-  },
-  password: {
-    type: String,
-  },
-  age: {
-    type: Number,
-  },
-  gender: {
-    type: String,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 //Now we create a mongoose model , model is a class create it's own instances when a new user comes
 // this model help in creating the new users.
